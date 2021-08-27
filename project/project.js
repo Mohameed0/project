c4014965e115a6b11ce30651c0a61416ef34c22f
Mohameed0 <88552256+Mohameed0@users.noreply.github.com>
2021-08-27 13:00:33 +0100

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,8 @@
+function text(x){
+    if (x==0) document.getElementById("mycode").style.display="block";
+    else document.getElementById("mycode").style.display="none";
+    return;
+}
+function myclick(){
+    document.location.href="../js/my booking.html";
+}
